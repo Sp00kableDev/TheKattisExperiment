97178a653edfa3e1bf9af7e6021c3deba77e4567
Sp00kable <62859356+Sp00kableDev@users.noreply.github.com>
2025-10-31 22:41:03 +0100

    Railroad kattis solved.

---
+++ RailRoad/RailRoad.cs
@@ -0,0 +1,27 @@
+﻿//Problem im solving:
+//https://open.kattis.com/problems/railroad2
+
+using System;
+
+public class RailRoad
+{
+
+    public static void Main(string[] args)
+    {
+
+        string input = Console.ReadLine();
+        string[] inputArray = input.Split(' ');
+        int y = int.Parse(inputArray[1]);
+
+        if (y % 2 == 1)
+        {
+            Console.WriteLine("impossible");
+        } 
+        else
+        {
+            Console.WriteLine("possible");
+        }
+        
+    }
+    
+}
